4340235a9dc012cacf4838ac2a2e028af339491a
naureen.imran <naureen.imran123@yahoo.com>
2022-05-08 20:44:23 +0100

    Working with files

---
+++ Day16/LearningPython10_1.py
@@ -0,0 +1,11 @@
+my_file = "learning_python.txt"
+
+with open (my_file) as my_object:
+    contents = my_object.read()
+
+print(contents)
+
+# With loop
+with open (my_file) as my_object:
+    for line in  my_object.read():
+        print(line.strip(''))
