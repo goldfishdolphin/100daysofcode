aeb03f686aa283961d47266d1303eda0ef35e577
naureen.imran <naureen.imran123@yahoo.com>
2022-05-10 16:51:44 +0100

    Conditional Logic in JS

---
+++ Day18/jsPractice.js
@@ -0,0 +1,10 @@
+// 7.4, 2.6, 234.2, 5.9, 63.5, -57.7, -200.5, 87.3
+
+// 1 - Round the above numbers to the nearest integer and log the result to the console
+const myNumbers = [7.4, 2.6, 234.2, 5.9, 63.5, -57.7, -200.5, 87.3];
+console.log(myNumbers);
+// 2 - Round the above numbers DOWN to the nearest integer and log the result to the console
+
+// 3 - Round the above numbers UP to the nearest integer and log the result to the console
+
+// You may wish to look at the MDN docs for Math.floor, Math.ceil & Math.round
---
+++ Day18/returnPassword.js
@@ -0,0 +1,12 @@
+//Write a function that takes a user object, 
+//and returns the value of key password. 
+//If there is no key of that name, it should return undefined.
+function retrievePassword (user) {
+    // Your code goes here...
+    let password=0;
+    
+    
+    return user.password;
+    }
+    
+    
