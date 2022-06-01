21c69ac0d878afc972b1ad3d50d53d28fac1210b
naureen.imran <naureen.imran123@yahoo.com>
2022-06-01 14:35:32 +0100

    Object

---
+++ Day40/tickettracking.js
@@ -0,0 +1,10 @@
+function trackAttendees(person, ticketCost) {
+    ticketCost = parseInt(ticketCost)
+    //console.log(ticketCost)
+  // Your code goes here...
+  
+  
+   person.paidForTicket=ticketCost;
+  return person
+  }
+  
