3b19861ccfca5e124dab39530946d7bb2fa2ca17
naureen.imran <naureen.imran123@yahoo.com>
2022-06-07 14:56:21 +0100

    Basic Scripting

---
+++ Day46/repeatstring.js
@@ -0,0 +1,14 @@
+function repeatStringNumTimes(str, num) {
+ 
+    let newString=''
+  for (let i=0; i<num; i++){
+ newString=newString+str ;
+  }
+  //console.log(newString)
+ return newString;
+  
+  }
+
+
+
+repeatStringNumTimes("abc", 3);
